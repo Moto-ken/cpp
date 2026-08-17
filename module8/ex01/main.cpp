634576b9cc955de370fcf212bb98f3abe37ffbf0
Moto-ken <motoken01315@gmail.com>
2026-08-17 15:49:24 +0900

    goood

--- module8/ex01/main.cpp
+++ module8/ex01/main.cpp
@@ -1,16 +1,19 @@
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 #include "Span.hpp"
 
 int main(void) {
-    Span sp = Span(5);
-    
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
+    Span sp = Span(10000);
+
+    std::srand(std::time(0));
+    for (int i = 0; i < 10000; ++i) {
+        // rand() returns a value between 0 and RAND_MAX on my system.
+        // RAND_MAX = 2147483647
+        sp.addNumber(std::rand());
+    }
 
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
